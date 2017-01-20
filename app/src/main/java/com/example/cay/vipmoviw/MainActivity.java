923e4e50213d43306ec5ee7ad265d7bc106da72e
Cay <276495166@qq.com>
2017-01-20 17:16:05 +0800

    第一次

---
+++ app/src/main/java/com/example/cay/vipmoviw/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.cay.vipmoviw;
+
+import android.databinding.DataBindingUtil;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.cay.vipmoviw.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // 一定需要对应的bean
+    private ActivityMainBinding mBinding;
+    private ImageView llTitleGank;
+    private ImageView llTitleOne;
+    private ImageView llTitleDou;
+
+    private NavigationView navView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initVivws();
+        initDrawerLayout();
+    }
+
+    private void initVivws() {
+        navView = mBinding.navView;
+
+    }
+
+    /**
+     * inflateHeaderView 进来的布局要宽一些
+     */
+    private void initDrawerLayout() {
+        navView.inflateHeaderView(R.layout.nav_header_main);
+        View headerView = navView.getHeaderView(0);
+//        LinearLayout viewById1 = (LinearLayout) headerView.findViewById(R.id.ll_header_bg);
+//        viewById1.setBackground();
+        ImageView ivAvatar = (ImageView) headerView.findViewById(R.id.iv_avatar);
+       // ImgLoadUtil.displayCircle(ivAvatar, R.drawable.ic_avatar);
+        LinearLayout llNavHomepage = (LinearLayout) headerView.findViewById(R.id.ll_nav_homepage);
+        LinearLayout llNavScanDownload = (LinearLayout) headerView.findViewById(R.id.ll_nav_scan_download);
+        LinearLayout llNavDeedback = (LinearLayout) headerView.findViewById(R.id.ll_nav_deedback);
+        LinearLayout llNavAbout = (LinearLayout) headerView.findViewById(R.id.ll_nav_about);
+        llNavHomepage.setOnClickListener(this);
+        llNavScanDownload.setOnClickListener(this);
+        llNavDeedback.setOnClickListener(this);
+        llNavAbout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
